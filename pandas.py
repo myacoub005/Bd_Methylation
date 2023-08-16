370245432b54d799c67b96738d0411dc7fe35c6b
Mark Yacoub <68869747+myacoub005@users.noreply.github.com>
2023-08-16 10:32:09 -0700

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -1,3 +1,7 @@
+base = pd.read_table("myfile.bed")
+base.loc[base['Methylation'] > 0, 'Methylation'] = 1
+base.to_csv('Methylation_above0.csv', sep=',', index = False)
+
 df = pd.read_csv('Methylation_above0_reduced_columns.tsv', sep=' ')
 
 c=pd.crosstab(df['Methylation'],df['gene_ID']
